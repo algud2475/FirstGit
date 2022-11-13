373cafc612e3cf1aea4c902103364a9a91c80f97
algud2475 <algud2475@yandex.ru>
2022-11-13 17:51:16 +0500

    Test1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Wow! It`s amazing!");
+        System.out.println("Why it`s not working?(");
+        System.out.println("1+2="+3);
     }
 }
